5d1601d046831e8b28d01949661e57b6403652f8
Sergio Luiz Paganini <slp7711@gmail.com>
2021-04-10 11:41:07 -0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo asperiores molestias vero distinctio enim harum incidunt earum quasi necessitatibus molestiae, fugiat blanditiis rerum sunt ipsum repellat officiis possimus, repudiandae hic!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo asperiores molestias vero distinctio enim harum incidunt earum quasi necessitatibus molestiae, fugiat blanditiis rerum sunt ipsum repellat officiis possimus, repudiandae hic!</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
